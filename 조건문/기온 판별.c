4869ee89574fd98bf734b4ef99a38d0b05633ce6
jjuhyeok <wngur0313@naver.com>
2022-04-24 07:44:55 +0900

    기온 판별

---
+++ 조건문/기온 판별.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+
+	int a;
+	scanf("%d", &a);
+
+	if (a < 0)
+		printf("cold, indoor");
+	else if (a >= 0 && a < 40)
+		printf("moderate, outdoor");
+	else
+		printf("hot, indoor");
+
+	return 0;
+}
